aaa152477fde79635fb67efb838c395f74796304
Lucas Tonolli <lucastonolli973@gmail.com>
2026-01-31 16:12:12 -0300

    feat: add Lists

---
+++ app/Models/Lists.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Lists extends Model
+{
+    protected $fillable = [
+        'title',
+        'owner_id',
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
