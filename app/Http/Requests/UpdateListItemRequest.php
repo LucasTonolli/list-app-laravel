4f8e8bbf0a4a6efb60af41b8d2fb208a8bbb7bb4
Lucas Tonolli <lucastonolli973@gmail.com>
2026-08-07 10:03:01 -0300

    refactor: remove unnecessary messages

--- app/Http/Requests/UpdateListItemRequest.php
+++ app/Http/Requests/UpdateListItemRequest.php
@@ -19,17 +19,4 @@ class UpdateListItemRequest extends FormRequest
             'version' => ['required', 'integer'],
         ];
     }
-
-    public function messages(): array
-    {
-        return [
-            'name.required' => 'Nome é obrigatório',
-            'name.string' => 'Nome deve ser uma string',
-            'name.max' => 'Nome deve ter no máximo 100 caracteres',
-            'description.string' => 'Descrição deve ser uma string',
-            'description.max' => 'Descrição deve ter no máximo 1000 caracteres',
-            'version.required' => 'Versão é obrigatório',
-            'version.integer' => 'Versão deve ser um inteiro',
-        ];
-    }
 }
