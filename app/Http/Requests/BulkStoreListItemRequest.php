c1f646f920157165df513873e9b54f62bf37281c
Lucas Tonolli <lucastonolli973@gmail.com>
2026-08-07 10:05:05 -0300

    docs: add comment explain why do not have description in bulkstore request

--- app/Http/Requests/BulkStoreListItemRequest.php
+++ app/Http/Requests/BulkStoreListItemRequest.php
@@ -9,7 +9,7 @@ class BulkStoreListItemRequest extends FormRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * Observation: quick mode, so we don't have description
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
