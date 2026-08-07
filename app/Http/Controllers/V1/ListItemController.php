a70db6a1e50a179fed1a985b823c22dc42fc8f2a
Lucas Tonolli <lucastonolli973@gmail.com>
2026-08-07 11:51:58 -0300

    fix: import exception name

--- app/Http/Controllers/V1/ListItemController.php
+++ app/Http/Controllers/V1/ListItemController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Exceptions\ItemversionMismatchException;
+use App\Exceptions\ItemVersionMismatchException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreListItemRequest;
 use App\Http\Requests\UpdateListItemRequest;
