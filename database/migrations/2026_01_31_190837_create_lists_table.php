be14d44702390d95c32a953614324f3aed543a1f
Lucas Tonolli <lucastonolli973@gmail.com>
2026-08-07 11:15:58 -0300

    fix: wrong table names in migrations

--- database/migrations/2026_01_31_190837_create_lists_table.php
+++ database/migrations/2026_01_31_190837_create_lists_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('custom_list');
+        Schema::dropIfExists('custom_lists');
     }
 };
